9f2c243e3dea5c900387a61f629203bf833b498b
Michael Quigley <michael@quigley.com>
2022-08-01 16:05:05 -0400

    more c-r-a lint (#14)

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,22 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="zrok">
+      <header className="zrok-header">
+        <h1>zrok</h1>
       </header>
     </div>
   );
