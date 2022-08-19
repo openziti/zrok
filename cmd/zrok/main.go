7da2311c6c3a269231603ae4fe2ee932a2c98f57
Michael Quigley <michael@quigley.com>
2022-08-19 10:44:02 -0400

    cmd lint

--- cmd/zrok/main.go
+++ cmd/zrok/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"github.com/go-openapi/runtime"
+	httptransport "github.com/go-openapi/runtime/client"
+	"github.com/go-openapi/strfmt"
 	"github.com/michaelquigley/pfxlog"
+	"github.com/openziti-test-kitchen/zrok/rest_client_zrok"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"os"
@@ -37,4 +41,11 @@ func main() {
 	if err := rootCmd.Execute(); err != nil {
 		panic(err)
 	}
+}
+
+func newZrokClient() *rest_client_zrok.Zrok {
+	transport := httptransport.New(endpoint, "/api/v1", nil)
+	transport.Producers["application/zrok.v1+json"] = runtime.JSONProducer()
+	transport.Consumers["application/zrok.v1+json"] = runtime.JSONConsumer()
+	return rest_client_zrok.New(transport, strfmt.Default)
 }
