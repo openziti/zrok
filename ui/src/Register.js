efee9cdd534a4fcb6bf6ec66dbe9f30e2bd2a31b
Michael Quigley <michael@quigley.com>
2022-09-19 15:46:49 -0400

    slightly better ui scaffolding for registration (#50)

--- ui/src/Register.js
+++ ui/src/Register.js
@@ -4,9 +4,16 @@ const Register = () => {
     const { token } = useParams();
 
     return (
-        <div>
-            <h1>Register!</h1>
-            <p>token = "{token}"</p>
+        <div className={"zrok"}>
+            <div className={"container"}>
+                <div className={"header"}>
+                    <img alt={"ziggy goes to space"} src="/ziggy.svg" width={"65px"} />
+                    <p className={"header-title"}>zrok</p>
+                </div>
+                <div className={"main"}>
+                    <h1>Register a new zrok account!</h1>
+                </div>
+            </div>
         </div>
     )
 }
