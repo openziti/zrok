01b43c6b74d14792d0b186a68c16e880acd81809
Michael Quigley <michael@quigley.com>
2022-11-11 10:50:17 -0500

    accidental hard-coded email sender address, corrected

--- controller/verify_email.go
+++ controller/verify_email.go
@@ -30,7 +30,7 @@ func sendVerificationEmail(emailAddress, token string) error {
 	}
 
 	msg := mail.NewMsg()
-	if err := msg.From("ziggy@zrok.io"); err != nil {
+	if err := msg.From(cfg.Registration.EmailFrom); err != nil {
 		return errors.Wrap(err, "failed to set from address in verification email")
 	}
 	if err := msg.To(emailAddress); err != nil {
