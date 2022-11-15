d661b6a5676a52a771de4b394f91697f7256b2dc
Michael Quigley <michael@quigley.com>
2022-11-15 14:48:40 -0500

    revised frontends schema (#20)

--- controller/store/sql/postgresql/002_v0_3_0_frontend_selection.sql
+++ controller/store/sql/postgresql/002_v0_3_0_frontend_selection.sql
@@ -1,13 +1,10 @@
 -- +migrate Up
 
-create type availability_type as enum ('public', 'private');
-
 create table frontends (
   id                    serial              primary key,
   environment_id        integer             references environments(id),
   z_id                  varchar(32)         not null unique,
   name                  varchar(64)         unique,
-  type                  availability_type   not null,
   created_at            timestamptz         not null default(current_timestamp),
   updated_at            timestamptz         not null default(current_timestamp)
 );
