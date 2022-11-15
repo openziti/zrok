cf77948ac57758982ca7defb9824864f261fd403
Michael Quigley <michael@quigley.com>
2022-11-15 11:04:05 -0500

    availability_type; frontends; environments.account_id nullale; (#20)

--- controller/store/sql/postgresql/002_ingress_selection.sql
+++ controller/store/sql/postgresql/002_ingress_selection.sql
@@ -1,9 +1,16 @@
 -- +migrate Up
 
-create table ingress (
+create type availability_type as enum ('public', 'private');
+
+create table frontends (
   id                    serial              primary key,
-  account_id            integer             references accounts(id),
+  environment_id        integer             references environments(id),
   z_id                  varchar(32)         not null unique,
+  type                  availability_type   not null,
   created_at            timestamptz         not null default(current_timestamp),
   updated_at            timestamptz         not null default(current_timestamp)
 );
+
+-- environments.account_id should allow NULL; environments with NULL account_id are "ephemeral"
+alter table environments drop constraint fk_accounts_identities;
+alter table environments add constraint fk_accounts_id foreign key (account_id) references accounts(id);
