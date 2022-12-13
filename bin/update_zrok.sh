67619df5c630f222e080d1028fa3617e80584afb
Michael Quigley <michael@quigley.com>
2022-12-13 12:22:59 -0500

    http-frontend -> access-public

--- bin/update_zrok.sh
+++ bin/update_zrok.sh
@@ -11,11 +11,11 @@ scp -i ~/.ssh/nf-zrok-ubuntu ~/local/zrok/bin/zrok ctrl-02.dev.zrok.io:local/zro
 ssh -i ~/.ssh/nf-zrok-ubuntu ctrl-02.dev.zrok.io sudo systemctl start zrok-ctrl
 
 # in-01.zrok.io
-ssh -i ~/.ssh/nf-zrok-ubuntu in-01.dev.zrok.io sudo systemctl stop zrok-http-frontend
+ssh -i ~/.ssh/nf-zrok-ubuntu in-01.dev.zrok.io sudo systemctl stop zrok-access-public
 scp -i ~/.ssh/nf-zrok-ubuntu ~/local/zrok/bin/zrok in-01.dev.zrok.io:local/zrok/bin/zrok
-ssh -i ~/.ssh/nf-zrok-ubuntu in-01.dev.zrok.io sudo systemctl start zrok-http-frontend
+ssh -i ~/.ssh/nf-zrok-ubuntu in-01.dev.zrok.io sudo systemctl start zrok-access-public
 
 # in-02.zrok.io
-ssh -i ~/.ssh/nf-zrok-ubuntu in-02.dev.zrok.io sudo systemctl stop zrok-http-frontend
+ssh -i ~/.ssh/nf-zrok-ubuntu in-02.dev.zrok.io sudo systemctl stop zrok-access-public
 scp -i ~/.ssh/nf-zrok-ubuntu ~/local/zrok/bin/zrok in-02.dev.zrok.io:local/zrok/bin/zrok
-ssh -i ~/.ssh/nf-zrok-ubuntu in-02.dev.zrok.io sudo systemctl start zrok-http-frontend
+ssh -i ~/.ssh/nf-zrok-ubuntu in-02.dev.zrok.io sudo systemctl start zrok-access-public
