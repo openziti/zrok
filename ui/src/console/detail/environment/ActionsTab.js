ee89cb6b36de84ce54a1c8cf17597feae4634de5
Michael Quigley <michael@quigley.com>
2023-01-05 15:25:30 -0500

    delete environment action (#142)

--- ui/src/console/detail/environment/ActionsTab.js
+++ ui/src/console/detail/environment/ActionsTab.js
@@ -1,6 +1,15 @@
 import {Button} from "react-bootstrap";
+import * as identity from "../../../api/environment";
 
 const ActionsTab = (props) => {
+    const deleteEnvironment = (envZId) => {
+        if(window.confirm("Really delete environment '" + envZId + "' and all shares within?")) {
+            identity.disable({body: {identity: envZId}}).then(resp => {
+                console.log(resp);
+            });
+        }
+    }
+
     return (
         <div class={"actions-tab"}>
             <h3>Delete your environment '{props.environment.description}' ({props.environment.zId})?</h3>
@@ -8,7 +17,7 @@ const ActionsTab = (props) => {
                 This will remove all shares from this environment, and will remove the environment from the network. You
                 will still need to terminate backends and <code>disable</code> your local environment.
             </p>
-            <Button variant={"danger"}>Delete '{props.environment.description}'</Button>
+            <Button variant={"danger"} onClick={() => deleteEnvironment(props.environment.zId)}>Delete '{props.environment.description}'</Button>
         </div>
     );
 };
