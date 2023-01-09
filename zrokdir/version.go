c4c497b88e770c4af9e3c0bb79602bc3b2f8fcac
Michael Quigley <michael@quigley.com>
2023-01-09 17:57:57 -0500

    mkdirall

--- zrokdir/version.go
+++ zrokdir/version.go
@@ -5,6 +5,7 @@ import (
 	"github.com/openziti-test-kitchen/zrok/tui"
 	"github.com/pkg/errors"
 	"os"
+	"path/filepath"
 )
 
 const V = "v0.3"
@@ -40,6 +41,9 @@ func writeMetadata() error {
 	}
 	data, err := json.Marshal(&Metadata{V: V})
 	if err != nil {
+		return err
+	}
+	if err := os.MkdirAll(filepath.Dir(mf), os.FileMode(0700)); err != nil {
 		return err
 	}
 	if err := os.WriteFile(mf, data, os.FileMode(0400)); err != nil {
