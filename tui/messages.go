3093bd230dc1e536b9ee263252f49adba7ffb131
Michael Quigley <michael@quigley.com>
2023-01-09 17:51:41 -0500

    formatting

--- tui/messages.go
+++ tui/messages.go
@@ -23,5 +23,5 @@ func Error(msg string, err error) {
 }
 
 func Warning(msg string, v ...interface{}) {
-	_, _ = fmt.Fprintf(os.Stderr, "%v: "+msg, append([]interface{}{WarningLabel}, v...))
+	_, _ = fmt.Fprintf(os.Stderr, "%v: "+msg+"\n", append([]interface{}{WarningLabel}, v...))
 }
