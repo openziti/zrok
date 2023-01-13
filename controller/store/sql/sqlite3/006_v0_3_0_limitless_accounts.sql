e8e87ca6ac042b71b0f0c6b2b8f4e1f7475372bb
Michael Quigley <michael@quigley.com>
2023-01-13 13:29:34 -0500

    fine, sqlite, fine... (#96)

--- controller/store/sql/sqlite3/006_v0_3_0_limitless_accounts.sql
+++ controller/store/sql/sqlite3/006_v0_3_0_limitless_accounts.sql
@@ -1,22 +1,3 @@
 -- +migrate Up
 
-alter table accounts rename to accounts_old;
-
-create table accounts (
-  id                    integer             primary key,
-  email                 string              not null unique,
-  password              string              not null,
-  token                 string              not null unique,
-  limitless             boolean             not null default(false),
-  created_at            datetime            not null default(strftime('%Y-%m-%d %H:%M:%f', 'now')),
-  updated_at            datetime            not null default(strftime('%Y-%m-%d %H:%M:%f', 'now')),
-
-  constraint chk_email check (email <> ''),
-  constraint chk_password check (password <> ''),
-  constraint chk_token check(token <> '')
-);
-
-insert into accounts (id, email, password, token, created_at, updated_at)
-    select id, email, password, token, created_at, updated_at from accounts_old;
-
-drop table accounts_old;
+alter table accounts add column limitless boolean not null default(false);
