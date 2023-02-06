f185959bef8df12a352c663985e297c60cfc275c
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2023-02-06 13:07:55 -0500

    enable algolia search

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -124,6 +124,29 @@ const config = {
           autoCollapseCategories: true,
         }
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'CO73R59OLO',
+  
+        // Public API key: it is safe to commit it
+        apiKey: '489572e91d0a750d34c127c2071ef962',
+  
+        indexName: 'zrok',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: 'external\\.example\\.com|thirdparty\\.example\\.com',
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
+      },
     }),
 };
 
