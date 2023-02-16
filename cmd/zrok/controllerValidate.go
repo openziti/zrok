ad09adc9412ac9ca4defb68f09e2d357c3883bdd
Michael Quigley <michael@quigley.com>
2023-02-16 13:51:47 -0500

    basic controller configuration validation (#238)

---
+++ cmd/zrok/controllerValidate.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/michaelquigley/cf"
+	"github.com/openziti/zrok/controller"
+	"github.com/openziti/zrok/tui"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	controllerCmd.cmd.AddCommand(newControllerValidateCommand().cmd)
+}
+
+type controllerValidateCommand struct {
+	cmd *cobra.Command
+}
+
+func newControllerValidateCommand() *controllerValidateCommand {
+	cmd := &cobra.Command{
+		Use:   "validate <configPath>",
+		Short: "Validate a zrok controlle configuration document",
+		Args:  cobra.ExactArgs(1),
+	}
+	command := &controllerValidateCommand{cmd: cmd}
+	cmd.Run = command.run
+	return command
+}
+
+func (cmd *controllerValidateCommand) run(_ *cobra.Command, args []string) {
+	cfg, err := controller.LoadConfig(args[0])
+	if err != nil {
+		tui.Error("controller config validation failed", err)
+	}
+	logrus.Infof(cf.Dump(cfg, cf.DefaultOptions()))
+}
