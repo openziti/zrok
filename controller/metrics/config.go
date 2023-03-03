e6dc836cc67dbf797591352444cc711c20bc0b28
Michael Quigley <michael@quigley.com>
2023-03-03 13:51:10 -0500

    more metrics infrastructure (#128)

--- controller/metrics/config.go
+++ controller/metrics/config.go
@@ -1,5 +1,18 @@
 package metrics
 
+import (
+	"github.com/michaelquigley/cf"
+	"github.com/pkg/errors"
+)
+
 type Config struct {
 	Source interface{}
+}
+
+func LoadConfig(path string) (*Config, error) {
+	cfg := &Config{}
+	if err := cf.BindYaml(cfg, path, GetCfOptions()); err != nil {
+		return nil, errors.Wrapf(err, "error loading config from '%v'", path)
+	}
+	return cfg, nil
 }
--- controller/metrics/fileSource.go
+++ controller/metrics/fileSource.go
@@ -1,17 +1,42 @@
 package metrics
 
-import "github.com/michaelquigley/cf"
+import (
+	"github.com/michaelquigley/cf"
+	"github.com/pkg/errors"
+	"os"
+)
 
 type FileSourceConfig struct {
 	Path string
 }
 
 func loadFileSourceConfig(v interface{}, opts *cf.Options) (interface{}, error) {
-	return nil, nil
+	if submap, ok := v.(map[string]interface{}); ok {
+		cfg := &FileSourceConfig{}
+		if err := cf.Bind(cfg, submap, cf.DefaultOptions()); err != nil {
+			return nil, err
+		}
+		return &fileSource{cfg}, nil
+	}
+	return nil, errors.New("invalid config structure for 'file' source")
 }
 
-type fileSource struct{}
+type fileSource struct {
+	cfg *FileSourceConfig
+}
 
 func (s *fileSource) Start() (chan struct{}, error) {
-	return nil, nil
+	f, err := os.Open(s.cfg.Path)
+	if err != nil {
+		return nil, errors.Wrapf(err, "error opening '%v'", s.cfg.Path)
+	}
+	ch := make(chan struct{})
+	go func() {
+		f.Close()
+		close(ch)
+	}()
+	return ch, nil
+}
+
+func (s *fileSource) Stop() {
 }
