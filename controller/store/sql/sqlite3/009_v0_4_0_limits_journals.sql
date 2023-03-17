1af440fa37e410e1305071225530c30d4f33c76d
Michael Quigley <michael@quigley.com>
2023-03-17 11:46:28 -0400

    limits journals ddl (#273)

---
+++ controller/store/sql/sqlite3/009_v0_4_0_limits_journals.sql
@@ -0,0 +1,25 @@
+-- +migrate Up
+
+create table account_limit_journal (
+    id                  serial                  primary key,
+    account_id          integer                 references accounts(id),
+    action              limit_action_type       not null,
+    created_at          timestamptz             not null default(current_timestamp),
+    updated_at          timestamptz             not null default(current_timestamp)
+);
+
+create table environment_limit_journal (
+    id                  serial                  primary key,
+    environment_id      integer                 references environments(id),
+    action              limit_action_type       not null,
+    created_at          timestamptz             not null default(current_timestamp),
+    updated_at          timestamptz             not null default(current_timestamp)
+);
+
+create table share_limit_journal (
+    id                  serial                  primary key,
+    share_id            integer                 references shares(id),
+    action              limit_action_type       not null,
+    created_at          timestamptz             not null default(current_timestamp),
+    updated_at          timestamptz             not null default(current_timestamp)
+);
