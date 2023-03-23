fdefa11db4af6d2d9ebc77af743ee5014831eead
Michael Quigley <michael@quigley.com>
2023-03-23 17:07:48 -0400

    strategy -> action (#276)

--- controller/limits/model.go
+++ controller/limits/model.go
@@ -2,14 +2,14 @@ package limits
 
 import "github.com/openziti/zrok/controller/store"
 
-type AccountStrategy interface {
+type AccountAction interface {
 	HandleAccount(a *store.Account, rxBytes, txBytes int64, limit *BandwidthPerPeriod) error
 }
 
-type EnvironmentStrategy interface {
+type EnvironmentAction interface {
 	HandleEnvironment(e *store.Environment, rxBytes, txBytes int64, limit *BandwidthPerPeriod) error
 }
 
-type ShareStrategy interface {
+type ShareAction interface {
 	HandleShare(s *store.Share, rxBytes, txBytes int64, limit *BandwidthPerPeriod) error
 }
