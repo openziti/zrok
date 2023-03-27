916251673ffb41c3499e50787057201c376a8655
Michael Quigley <michael@quigley.com>
2023-03-27 11:53:18 -0400

    account action skeletons (#276)

---
+++ controller/limits/accounttWarningAction.go
@@ -0,0 +1,21 @@
+package limits
+
+import (
+	"github.com/openziti/edge/rest_management_api_client"
+	"github.com/openziti/zrok/controller/store"
+	"github.com/sirupsen/logrus"
+)
+
+type accountWarningAction struct {
+	str  *store.Store
+	edge *rest_management_api_client.ZitiEdgeManagement
+}
+
+func newAccountWarningAction(str *store.Store, edge *rest_management_api_client.ZitiEdgeManagement) *accountWarningAction {
+	return &accountWarningAction{str, edge}
+}
+
+func (a *accountWarningAction) HandleAccount(acct *store.Account, rxBytes, txBytes int64, limit *BandwidthPerPeriod) error {
+	logrus.Infof("warning '%v'", acct.Email)
+	return nil
+}
