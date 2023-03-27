bb61bdb664147e04fcefa15c8919bbf6ed4d2315
Michael Quigley <michael@quigley.com>
2023-03-27 15:07:09 -0400

    limit warning emails (#276)

--- controller/emailUi/embed.go
+++ controller/emailUi/embed.go
@@ -2,5 +2,5 @@ package emailUi
 
 import "embed"
 
-//go:embed verify.gohtml verify.gotext resetPassword.gohtml resetPassword.gotext
+//go:embed verify.gohtml verify.gotext resetPassword.gohtml resetPassword.gotext limitWarning.gohtml limitWarning.gotext
 var FS embed.FS
