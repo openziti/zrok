bd4ce22d971e1bd322da0dcc4d16fb2b8c73c473
Michael Quigley <michael@quigley.com>
2023-03-28 15:05:51 -0400

    don't try to replace policies for deleted shares (#276)

--- controller/limits/environmentRelaxAction.go
+++ controller/limits/environmentRelaxAction.go
@@ -27,24 +27,26 @@ func (a *environmentRelaxAction) HandleEnvironment(env *store.Environment, rxByt
 	}
 
 	for _, shr := range shrs {
-		if shr.ShareMode == "public" {
-			env, err := a.str.GetEnvironment(shr.EnvironmentId, trx)
-			if err != nil {
-				return errors.Wrap(err, "error finding environment")
-			}
+		if !shr.Deleted {
+			if shr.ShareMode == "public" {
+				env, err := a.str.GetEnvironment(shr.EnvironmentId, trx)
+				if err != nil {
+					return errors.Wrap(err, "error finding environment")
+				}
 
-			fe, err := a.str.FindFrontendPubliclyNamed(*shr.FrontendSelection, trx)
-			if err != nil {
-				return errors.Wrapf(err, "error finding frontend name '%v' for '%v'", *shr.FrontendSelection, shr.Token)
-			}
+				fe, err := a.str.FindFrontendPubliclyNamed(*shr.FrontendSelection, trx)
+				if err != nil {
+					return errors.Wrapf(err, "error finding frontend name '%v' for '%v'", *shr.FrontendSelection, shr.Token)
+				}
 
-			if err := zrokEdgeSdk.CreateServicePolicyDial(env.ZId+"-"+shr.ZId+"-dial", shr.ZId, []string{fe.ZId}, zrokEdgeSdk.ZrokShareTags(shr.Token).SubTags, a.edge); err != nil {
-				return errors.Wrapf(err, "error creating dial service policy for '%v'", shr.Token)
-			}
-			logrus.Infof("added dial service policy for '%v'", shr.Token)
+				if err := zrokEdgeSdk.CreateServicePolicyDial(env.ZId+"-"+shr.ZId+"-dial", shr.ZId, []string{fe.ZId}, zrokEdgeSdk.ZrokShareTags(shr.Token).SubTags, a.edge); err != nil {
+					return errors.Wrapf(err, "error creating dial service policy for '%v'", shr.Token)
+				}
+				logrus.Infof("added dial service policy for '%v'", shr.Token)
 
-		} else if shr.ShareMode == "private" {
-			return errors.New("share relax for private share not implemented")
+			} else if shr.ShareMode == "private" {
+				return errors.New("share relax for private share not implemented")
+			}
 		}
 	}
 
