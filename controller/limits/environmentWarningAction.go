d3be3195bfe3685b041dd2a51502bf05151dfaae
Michael Quigley <michael@quigley.com>
2023-03-29 16:14:57 -0400

    improved warning email text (#279)

--- controller/limits/environmentWarningAction.go
+++ controller/limits/environmentWarningAction.go
@@ -1,10 +1,12 @@
 package limits
 
 import (
+	"fmt"
 	"github.com/jmoiron/sqlx"
 	"github.com/openziti/edge/rest_management_api_client"
 	"github.com/openziti/zrok/controller/emailUi"
 	"github.com/openziti/zrok/controller/store"
+	"github.com/openziti/zrok/util"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
@@ -28,7 +30,23 @@ func (a *environmentWarningAction) HandleEnvironment(env *store.Environment, rxB
 			return err
 		}
 
-		if err := sendLimitWarningEmail(a.cfg, acct.Email, limit, rxBytes, txBytes); err != nil {
+		rxLimit := "unlimited bytes"
+		if limit.Limit.Rx != Unlimited {
+			rxLimit = util.BytesToSize(limit.Limit.Rx)
+		}
+		txLimit := "unlimited bytes"
+		if limit.Limit.Tx != Unlimited {
+			txLimit = util.BytesToSize(limit.Limit.Tx)
+		}
+		totalLimit := "unlimited bytes"
+		if limit.Limit.Total != Unlimited {
+			totalLimit = util.BytesToSize(limit.Limit.Total)
+		}
+		detail := fmt.Sprintf("Your environment '%v' has received %v and sent %v (for a total of %v), which has triggered a transfer limit warning.", env.Description, util.BytesToSize(rxBytes), util.BytesToSize(txBytes), util.BytesToSize(rxBytes+txBytes)) +
+			fmt.Sprintf(" This zrok instance only allows a share to receive %v, send %v, totalling not more than %v for each %v.", rxLimit, txLimit, totalLimit, limit.Period) +
+			fmt.Sprintf(" If you exceed the transfer limit, access to your shares will be temporarily disabled (until the last %v falls below the transfer limit).", limit.Period)
+
+		if err := sendLimitWarningEmail(a.cfg, acct.Email, detail); err != nil {
 			return errors.Wrapf(err, "error sending limit warning email to '%v'", acct.Email)
 		}
 	}
