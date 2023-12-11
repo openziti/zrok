d4c005ccf3840b5c001bea6354a297d97f3383b8
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2023-12-11 18:05:29 -0500

    normalize var names with "ZROK_" prefix

--- nfpm/zrok-share.bash
+++ nfpm/zrok-share.bash
@@ -110,8 +110,8 @@ case "${ZROK_BACKEND_MODE}" in
     ;;
 esac
 
-[[ -n "${UNIQUE_NAME:-}" ]] && {
-  ZROK_CMD+=" --unique-name ${UNIQUE_NAME}"
+[[ -n "${ZROK_UNIQUE_NAME:-}" ]] && {
+  ZROK_CMD+=" --unique-name ${ZROK_UNIQUE_NAME}"
 }
 
 ZROK_CMD+=" --backend-mode ${ZROK_BACKEND_MODE} ${ZROK_TARGET}"
