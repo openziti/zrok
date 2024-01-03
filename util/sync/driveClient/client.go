ef0ac1e140a7bf0475de133c11961898f02f459d
Michael Quigley <michael@quigley.com>
2024-01-03 17:05:18 -0500

    snapshot

--- util/sync/driveClient/client.go
+++ util/sync/driveClient/client.go
@@ -6,10 +6,27 @@ type Client struct {
 	client *http.Client
 }
 
-func NewHttpClient(uri string) *Client {
+func NewHttpClient() *Client {
 	return &Client{&http.Client{}}
 }
 
 func (c *Client) Connect() error {
 	return nil
+}
+
+func (c *Client) options(uri string) (*http.Response, error) {
+	return c.request("OPTIONS", uri)
+}
+
+func (c *Client) request(method, uri string) (resp *http.Response, err error) {
+	req, err := http.NewRequest(method, uri, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp, err = c.client.Do(req); err != nil {
+		return resp, err
+	}
+
+	return resp, err
 }
