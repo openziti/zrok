f77404b4db6c4769f60e83348a27f8c3eca53870
Cam <otts.cameron@gmail.com>
2024-01-25 09:58:55 -0600

    update casing

--- controller/store/sql/postgresql/017_v0_4_23_password_reset_request_unique.sql
+++ controller/store/sql/postgresql/017_v0_4_23_password_reset_request_unique.sql
@@ -4,4 +4,4 @@
 ALTER TABLE password_reset_requests DROP CONSTRAINT password_reset_requests_account_id_key;
 
 -- add new constraint which doesnt mind having multiple resets for account ids
-ALTER TABLE password_reset_requests ADD CONSTRAINT password_reset_requests_account_id_key FOREIGN KEY (account_id) REFERENCES accounts (id) on delete cascade;
+ALTER TABLE password_reset_requests ADD CONSTRAINT password_reset_requests_account_id_key FOREIGN KEY (account_id) REFERENCES accounts (id) ON DELETE CASCADE;
