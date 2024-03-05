18827838920660686b077e13ecccb4f29a7d1101
Michael Quigley <michael@quigley.com>
2024-03-05 12:44:56 -0500

    wiring up the private share cli to the permission mode; sqlite tweak (#432)

--- controller/store/sql/sqlite3/019_v0_4_26_permission_model.sql
+++ controller/store/sql/sqlite3/019_v0_4_26_permission_model.sql
@@ -3,7 +3,7 @@
 alter table shares add column permission_mode string not null default('open');
 
 create table access_grants (
-    id                  serial                  primary key,
+    id                  integer                 primary key,
     share_id            integer                 references shares(id),
     account_id          integer                 references accounts(id),
     created_at          datetime                not null default(strftime('%Y-%m-%d %H:%M:%f', 'now')),
