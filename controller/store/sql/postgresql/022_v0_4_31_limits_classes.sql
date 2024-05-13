5d72890d320d5a01da180edb6c450951d7ba4c1a
Michael Quigley <michael@quigley.com>
2024-05-13 12:36:00 -0400

    limits ddl for postgres (#606)

---
+++ controller/store/sql/postgresql/022_v0_4_31_limits_classes.sql
@@ -0,0 +1,19 @@
+-- +migrate Up
+
+create type limit_scope as enum ('account', 'environment', 'share');
+create type limit_action as enum ('warning', 'limit');
+
+create table limits_classes (
+    id                  serial                  primary key,
+    limit_scope         limit_scope             not null default ('account'),
+    limit_action        limit_action            not null default ('limit'),
+    share_mode          share_mode,
+    backend_mode        backend_mode,
+    period_minutes      int                     not null default (1440),
+    rx_bytes            bigint                  not null default (-1),
+    tx_bytes            bigint                  not null default (-1),
+    total_bytes         bigint                  not null default (-1),
+    created_at          timestamptz             not null default(current_timestamp),
+    updated_at          timestamptz             not null default(current_timestamp),
+    deleted             boolean                 not null default(false)
+)
