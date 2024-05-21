f23f2495ef9ee42122ba5dbbd3cc642a83af1832
Michael Quigley <michael@quigley.com>
2024-05-21 15:16:56 -0400

    overrides for enviroments and shares (#606); overrides for reserved shares and unique names (#632)

--- controller/store/sql/postgresql/022_v0_4_31_limits_classes.sql
+++ controller/store/sql/postgresql/022_v0_4_31_limits_classes.sql
@@ -5,14 +5,21 @@ create type limit_action as enum ('warning', 'limit');
 
 create table limit_classes (
     id                  serial                  primary key,
+
     limit_scope         limit_scope             not null default ('account'),
     limit_action        limit_action            not null default ('limit'),
     share_mode          share_mode,
     backend_mode        backend_mode,
+
+    environments        int                     not null default (-1),
+    shares              int                     not null default (-1),
+    reserved_shares     int                     not null default (-1),
+    unique_names        int                     not null default (-1),
     period_minutes      int                     not null default (1440),
     rx_bytes            bigint                  not null default (-1),
     tx_bytes            bigint                  not null default (-1),
     total_bytes         bigint                  not null default (-1),
+
     created_at          timestamptz             not null default(current_timestamp),
     updated_at          timestamptz             not null default(current_timestamp),
     deleted             boolean                 not null default(false)
--- controller/store/sql/sqlite3/022_v0_4_31_limits_classes.sql
+++ controller/store/sql/sqlite3/022_v0_4_31_limits_classes.sql
@@ -2,14 +2,21 @@
 
 create table limit_classes (
     id                  serial                  primary key,
+
     limit_scope         string                  not null default ('account'),
     limit_action        string                  not null default ('limit'),
     share_mode          string,
     backend_mode        string,
+
+    environments        integer                 not null default (-1),
+    shares              integer                 not null default (-1),
+    reserved_shares     integer                 not null default (-1),
+    unique_names        integer                 not null default (-1),
     period_minutes      integer                 not null default (1440),
     rx_bytes            bigint                  not null default (-1),
     tx_bytes            bigint                  not null default (-1),
     total_bytes         bigint                  not null default (-1),
+
     created_at          datetime                not null default(strftime('%Y-%m-%d %H:%M:%f', 'now')),
     updated_at          datetime                not null default(strftime('%Y-%m-%d %H:%M:%f', 'now')),
     deleted             boolean                 not null default(false)
