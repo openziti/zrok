d2d3adc5ccb15292e83823be303073ebbcef1c1a
Michael Quigley <michael@quigley.com>
2024-05-31 14:32:02 -0400

    sqlite schema tweaks for bandwidth_limit_journal (#606)

--- controller/store/sql/sqlite3/025_v0_4_31_bandwidth_limit_journal.sql
+++ controller/store/sql/sqlite3/025_v0_4_31_bandwidth_limit_journal.sql
@@ -11,6 +11,8 @@ create table bandwidth_limit_journal (
     action              string                  not null,
     rx_bytes            bigint                  not null,
     tx_bytes            bigint                  not null,
-    created_at          timestamptz             not null default(current_timestamp),
-    updated_at          timestamptz             not null default(current_timestamp)
+    created_at          timestamptz             not null default(strftime('%Y-%m-%d %H:%M:%f', 'now')),
+    updated_at          timestamptz             not null default(strftime('%Y-%m-%d %H:%M:%f', 'now'))
 );
+
+create index bandwidth_limit_journal_account_id_idx on bandwidth_limit_journal (account_id);
