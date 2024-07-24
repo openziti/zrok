275666c2b23f3e8d8b80752e258d0c0a3090e649
Michael Quigley <michael@quigley.com>
2024-07-24 11:16:36 -0400

    skip_interstitial_grants sql structure (#704)

---
+++ controller/store/skipInterstitialGrant.go
@@ -0,0 +1,18 @@
+package store
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+)
+
+func (str *Store) IsAccountGrantedSkipInterstitial(acctId int, trx *sqlx.Tx) (bool, error) {
+	stmt, err := trx.Prepare("select count(0) from skip_interstitial_grants where account_id = $1")
+	if err != nil {
+		return false, errors.Wrap(err, "error preparing skip_interstitial_grants select statement")
+	}
+	var count int
+	if err := stmt.QueryRow(acctId).Scan(&count); err != nil {
+		return false, errors.Wrap(err, "error querying skip_interstitial_grants count")
+	}
+	return count > 0, nil
+}
