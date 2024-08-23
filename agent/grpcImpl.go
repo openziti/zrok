d8c9681da1ce52bc5718c21ef5c644f834217600
Michael Quigley <michael@quigley.com>
2024-08-23 12:41:13 -0400

    tweak (#463)

--- agent/grpcImpl.go
+++ agent/grpcImpl.go
@@ -4,13 +4,15 @@ import (
 	"context"
 	"github.com/openziti/zrok/agent/agentGrpc"
 	"github.com/openziti/zrok/build"
+	"github.com/sirupsen/logrus"
 )
 
 type agentGrpcImpl struct {
 	agentGrpc.UnimplementedAgentServer
 }
 
-func (s *agentGrpcImpl) Version(ctx context.Context, req *agentGrpc.VersionRequest) (*agentGrpc.VersionReply, error) {
+func (s *agentGrpcImpl) Version(_ context.Context, _ *agentGrpc.VersionRequest) (*agentGrpc.VersionReply, error) {
 	v := build.String()
+	logrus.Infof("responding to version inquiry with '%v'", v)
 	return &agentGrpc.VersionReply{V: &v}, nil
 }
