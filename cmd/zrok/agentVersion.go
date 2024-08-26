e0592a3625d2d4c62b217cee93849675900ecd54
Michael Quigley <michael@quigley.com>
2024-08-26 11:49:28 -0400

    factor out unix domain grpc client (#463)

--- cmd/zrok/agentVersion.go
+++ cmd/zrok/agentVersion.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"context"
+	"github.com/openziti/zrok/agent/agentClient"
 	"github.com/openziti/zrok/agent/agentGrpc"
 	"github.com/openziti/zrok/environment"
 	"github.com/openziti/zrok/tui"
 	"github.com/spf13/cobra"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/resolver"
-	"net"
 )
 
 func init() {
@@ -37,24 +34,11 @@ func (cmd *agentVersionCommand) run(_ *cobra.Command, _ []string) {
 		tui.Error("error loading zrokdir", err)
 	}
 
-	agentSocket, err := root.AgentSocket()
+	client, conn, err := agentClient.NewClient(root)
 	if err != nil {
-		tui.Error("error getting agent socket", err)
-	}
-
-	opts := []grpc.DialOption{
-		grpc.WithContextDialer(func(_ context.Context, addr string) (net.Conn, error) {
-			return net.Dial("unix", addr)
-		}),
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-	}
-	resolver.SetDefaultScheme("passthrough")
-	conn, err := grpc.NewClient(agentSocket, opts...)
-	if err != nil {
-		tui.Error("error connecting to agent socket", err)
+		tui.Error("error getting agent client", err)
 	}
 	defer conn.Close()
-	client := agentGrpc.NewAgentClient(conn)
 
 	v, err := client.Version(context.Background(), &agentGrpc.VersionRequest{})
 	if err != nil {
