f0cd66dbe2a0a62e3c918a37e15df12673cdc25f
Michael Quigley <michael@quigley.com>
2024-10-08 14:47:21 -0400

    route parmeters (#221)

--- agent/agentUi/src/ShareDetail.jsx
+++ agent/agentUi/src/ShareDetail.jsx
@@ -1,10 +1,14 @@
 import NavBar from "./NavBar.jsx";
+import {useParams} from "react-router-dom";
 
 function ShareDetail() {
+    let params = useParams();
+
     return (
         <>
             <NavBar />
-            <h1>Share Detail</h1>
+
+            <h1>Share {params.token}</h1>
         </>
     )
 }
--- agent/agentUi/src/main.jsx
+++ agent/agentUi/src/main.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
         element: <Overview />
     },
     {
-        path: "/share",
+        path: "/share/:token",
         element: <ShareDetail />
     }
 ]);
