e34e66172272b1018ef49d317d56adf46b0f6a09
Michael Quigley <michael@quigley.com>
2024-10-29 11:39:18 -0400

    handler tweaks (#221)

--- agent/agentUi/src/model/handler.js
+++ agent/agentUi/src/model/handler.js
@@ -1,39 +1,49 @@
 import {AgentApi, ApiClient} from "../api/src/index.js";
 
 export const getAgentApi = () => {
-    return new AgentApi(new ApiClient("http://localhost:5173"));
+    return new AgentApi(new ApiClient(window.location.origin));
 }
 
 export const createShare = (opts) => {
     switch(opts.shareMode) {
         case "public":
             getAgentApi().agentSharePublic(opts, (e, d) => {
-                console.log("createShare", e, d);
+                if(e) {
+                    console.log("createShare", e, d);
+                }
             });
             break;
 
         case "private":
             getAgentApi().agentSharePrivate(opts, (e, d) => {
-                console.log("createShare", e, d);
-            })
+                if(e) {
+                    console.log("createShare", e, d);
+                }
+            });
             break;
     }
 }
 
 export const releaseShare = (opts) => {
     getAgentApi().agentReleaseShare(opts, (e, d) => {
-        console.log("releaseShare", e, d);
-    })
+        if(e) {
+            console.log("releaseShare", e, d);
+        }
+    });
 }
 
 export const createAccess = (opts) => {
     getAgentApi().agentAccessPrivate(opts, (e, d) => {
-        console.log("createAccess", e, d);
-    })
+        if(e) {
+            console.log("createAccess", e, d);
+        }
+    });
 }
 
 export const releaseAccess = (opts) => {
-    getAgentApi().agentReleaseAccess(opts, (e, d) => {
-        console.log("releaseAccess", e, d);
-    })
+    if(e) {
+        getAgentApi().agentReleaseAccess(opts, (e, d) => {
+            console.log("releaseAccess", e, d);
+        });
+    }
 }
