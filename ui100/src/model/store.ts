686443c921f219e6bc8d15023bd354a69c7bc4c2
Michael Quigley <michael@quigley.com>
2024-12-18 14:38:06 -0500

    extraneous store cruft (#809)

--- ui100/src/model/store.ts
+++ ui100/src/model/store.ts
@@ -1,20 +1,16 @@
 import {create} from "zustand";
-import {Environment, Metrics} from "../api";
+import {Environment} from "../api";
 
 type State = {
-    account: Metrics;
     environments: Array<Environment>;
 };
 
 type Action = {
-    updateAccount: (metrics: State['account']) => void
     updateEnvironments: (environments: State['environments']) => void
 };
 
 const useMetricsStore = create<State & Action>((set) => ({
-    account: null,
     environments: new Array<Environment>(),
-    updateAccount: (metrics) => set({account: metrics}),
     updateEnvironments: (environments) => set({environments: environments}),
 }));
 
