2dbd6a520dc447771317496cef9f6e7d508b0fed
Michael Quigley <michael@quigley.com>
2024-12-18 12:05:43 -0500

    deal with dynamic handles later (#799)

--- ui100/src/EnvironmentNode.tsx
+++ ui100/src/EnvironmentNode.tsx
@@ -3,14 +3,10 @@ import {Grid2} from "@mui/material";
 import EnvironmentIcon from "@mui/icons-material/Computer";
 
 const EnvironmentNode = ({ data }) => {
-    let shareHandle = <Handle type="source" position={Position.Bottom} />;
-    if(data.empty) {
-        shareHandle = <></>;
-    }
     return (
         <>
             <Handle type="target" position={Position.Top} />
-            {shareHandle}
+            <Handle type="source" position={Position.Bottom} />
             <Grid2 container sx={{ flexGrow: 1, p: 1 }} alignItems="center">
                 <Grid2 display="flex"><EnvironmentIcon sx={{ fontSize: 15, mr: 0.5 }}/></Grid2>
                 <Grid2 display="flex">{data.label}</Grid2>
