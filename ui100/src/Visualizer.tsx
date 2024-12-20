b419300ea2789bd68fdeb1574fa23106647e2c44
Michael Quigley <michael@quigley.com>
2024-12-20 13:01:22 -0500

    moved styling components (#724)

--- ui100/src/Visualizer.tsx
+++ ui100/src/Visualizer.tsx
@@ -1,5 +1,5 @@
 import "@xyflow/react/dist/style.css";
-import "./react-flow.css";
+import "./styling/react-flow.css";
 import {
     Background,
     Controls,
--- ui100/src/main.tsx
+++ ui100/src/main.tsx
@@ -1,8 +1,8 @@
-import "./index.css";
+import "./styling/index.css";
 import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 import {ThemeProvider} from "@mui/material";
-import {theme} from "./model/theme.ts";
+import {theme} from "./styling/theme.ts";
 import App from "./App.tsx";
 
 createRoot(document.getElementById('root')!).render(
