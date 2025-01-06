7b53ad066181c3e4bde77e8892b763362ebd09f9
Michael Quigley <michael@quigley.com>
2025-01-06 11:51:29 -0500

    improved feels PropertyTable (#804)

--- ui100/src/PropertyTable.tsx
+++ ui100/src/PropertyTable.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {camelToWords, objectToRows} from "./model/util.ts";
-import {Paper, Table, TableBody, TableCell, TableHead, TableRow} from "@mui/material";
+import {Paper, Table, TableBody, TableCell, TableRow} from "@mui/material";
 
 type PropertyTableProps = {
     object: any;
@@ -26,16 +26,10 @@ const PropertyTable = ({ object, custom }: PropertyTableProps) => {
     return (
         <Paper>
             <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Property</TableCell>
-                        <TableCell>Value</TableCell>
-                    </TableRow>
-                </TableHead>
                 <TableBody>
                     {data.map((row) => (
                         <TableRow key={row.id}>
-                            <TableCell sx={{ width: 100 }}>{camelToWords(row.property)}</TableCell>
+                            <TableCell sx={{ width: 100 }}><strong>{camelToWords(row.property)}</strong></TableCell>
                             <TableCell sx={{ width: 1000 }}>{value(row)}</TableCell>
                         </TableRow>
                     ))}
