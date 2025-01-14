96c556f657a8069724b602e2ddd61a8125f88264
Michael Quigley <michael@quigley.com>
2025-01-14 13:49:47 -0500

    remove hamburger from agentUi, also

--- agent/agentUi/src/NavBar.tsx
+++ agent/agentUi/src/NavBar.tsx
@@ -1,23 +1,18 @@
-import {AppBar, Box, Button, Grid2, IconButton, Toolbar, Typography} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import {AppBar, Box, Button, Grid2, Toolbar, Typography} from "@mui/material";
 import LanIcon from "@mui/icons-material/Lan";
 import ShareIcon from "@mui/icons-material/Share";
 import zroket from "./assets/zrok-1.0.0-rocket-green.svg";
 
 interface NavBarProps {
-    version: string;
     shareClick: () => void;
     accessClick: () => void;
 }
 
-const NavBar = ({ version, shareClick, accessClick }: NavBarProps) => {
+const NavBar = ({ shareClick, accessClick }: NavBarProps) => {
     return (
         <Box ssx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                        <MenuIcon />
-                    </IconButton>
                     <Typography variant="h6" sx={{ flexGrow: 1 }}>
                         <Grid2 container sx={{ flexGrow: 1 }}>
                             <Grid2 display="flex" justifyContent="left">
