3751d0b75ac3e5fc90c25140bd8fc5a075246826
Michael Quigley <michael@quigley.com>
2025-01-21 14:18:05 -0500

    better l&f (#822)

--- ui100/src/EnvironmentPanel.tsx
+++ ui100/src/EnvironmentPanel.tsx
@@ -43,13 +43,13 @@ const ReleaseEnvironmentModal = ({ close, isOpen, detail, action }: ReleaseEnvir
                     <Typography variant="h5"><strong>Release Environment</strong></Typography>
                 </Grid2>
                 <Grid2 container sx={{ flexGrow: 1, p: 1 }} alignItems="center">
-                    <Typography variant="body1">Would you like to release the environment <strong>'{description}'</strong>?</Typography>
+                    <Typography variant="body1">Would you like to release the environment <code>{description}</code> ?</Typography>
                 </Grid2>
                 <Grid2 container sx={{ flexGrow: 1, p: 1 }} alignItems="center">
                     <Typography variant="body1">Releasing this environment will also release any shares and accesses that are associated with it.</Typography>
                 </Grid2>
                 <Grid2 container sx={{ flexGrow: 1, p: 1 }} alignItems="center">
-                    <FormControlLabel control={<Checkbox checked={checked} onChange={toggleChecked} />} label={"I am ready to release '" + description + "'"} sx={{ mt: 2 }} />
+                    <FormControlLabel control={<Checkbox checked={checked} onChange={toggleChecked} />} label={<p>I confirm the release of <code>{description}</code></p>} sx={{ mt: 2 }} />
                 </Grid2>
                 <Grid2 container sx={{ flexGrow: 1 }} alignItems="center">
                     <Button color="error" variant="contained" disabled={!checked} onClick={action}>Release</Button>
