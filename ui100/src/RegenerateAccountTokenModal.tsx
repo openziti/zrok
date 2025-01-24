2b998e1420ac3d7072a123b4703dc8a08b299f66
Michael Quigley <michael@quigley.com>
2025-01-24 13:43:43 -0500

    regenerate account token text improvements (#822)

--- ui100/src/RegenerateAccountTokenModal.tsx
+++ ui100/src/RegenerateAccountTokenModal.tsx
@@ -29,19 +29,32 @@ const RegenerateAccountTokenModal = ({ close, isOpen, user }: RegenerateAccountT
                     <Typography variant="h5"><strong>Regenerate Account Token</strong></Typography>
                 </Grid2>
                 <Grid2 container sx={{ flexGrow: 1, p: 1 }} alignItems="center">
-                    <Typography>Regenerating your account token will stop all environments and shares from operating properly!</Typography>
+                    <Typography variant="h6" color="red">
+                        WARNING: Regenerating your account token will stop all environments and shares from operating properly!
+                        Please read the following instructions to prevent interruptions!
+                    </Typography>
                 </Grid2>
                 <Grid2 container sx={{ flexGrow: 1, p: 1 }} alignItems="center">
-                    <Typography>You will need to manually edit your $HOME/.zrok/environment.json files (in each environment) to use the new zrok_token. Updating these files will restore the functionality of your environments.</Typography>
+                    <Typography>
+                        You will need to manually edit your <code>$&#123;HOME&#125;/.zrok/environment.json</code> files
+                        (in each environment) to use the new <code>zrok_token</code>. Updating these files will restore
+                        the functionality of your environments.
+                    </Typography>
                 </Grid2>
                 <Grid2 container sx={{ flexGrow: 1, p: 1 }} alignItems="center">
-                    <Typography>Alternatively, you can just zrok disable any enabled environments and re-enable using the new account token. Running zrok disable will delete your environments and any shares they contain (including reserved shares). So if you have environments and reserved shares you need to preserve, your best option is to update the zrok_token in those environments as described above.</Typography>
+                    <Typography>
+                        Alternatively, you can just <code>zrok disable</code> any enabled environments and re-enable
+                        using the updated account token. Running <code>zrok disable</code> before you regenerate will
+                        delete your environments and any shares they contain (including reserved shares). So if you have
+                        environments and reserved shares you need to preserve, your best option is to update the
+                        <code>zrok_token</code> in those environments as described above.
+                    </Typography>
                 </Grid2>
                 <Grid2 container sx={{ flexGrow: 1, p: 1 }} alignItems="center">
                     <FormControlLabel control={<Checkbox checked={checked} onChange={toggleChecked} />} label={<p>I confirm that I want to regenerate my account token</p>} sx={{ mt: 2 }} />
                 </Grid2>
                 <Grid2 container sx={{ flexGrow: 1 }} alignItems="center">
-                    <Button color="error" variant="contained" disabled={!checked}>Release</Button>
+                    <Button color="error" variant="contained" disabled={!checked}>Regenerate Account Token</Button>
                 </Grid2>
             </Box>
         </Modal>
