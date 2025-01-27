f962a7d253238baaa84ae012ae725bf14ed0c512
Michael Quigley <michael@quigley.com>
2025-01-27 13:48:29 -0500

    cracked the approach for live form editing; password change (#822)

--- ui100/src/AccountPasswordChangeModal.tsx
+++ ui100/src/AccountPasswordChangeModal.tsx
@@ -1,8 +1,10 @@
 import {User} from "./model/user.ts";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {modalStyle} from "./styling/theme.ts";
 import {Box, Button, Grid2, Modal, TextField, Typography} from "@mui/material";
 import {useFormik} from "formik";
+import * as Yup from 'yup';
+import {getAccountApi} from "./model/api.ts";
 
 interface AccountPasswordChangeModalProps {
     close: () => void;
@@ -12,6 +14,8 @@ interface AccountPasswordChangeModalProps {
 
 const AccountPasswordChangeModal =({ close, isOpen, user }: AccountPasswordChangeModalProps) => {
     const [errorMessage, setErrorMessage] = useState<React.JSX.Element>(null);
+    const submitButton = <Button color="primary" variant="contained" type="submit" sx={{ mt: 2 }}>Change Password</Button>;
+    const [bottomControl, setBottomControl] = useState<React.JSX.Element>(submitButton);
 
     const passwordChangeForm = useFormik({
         initialValues: {
@@ -20,10 +24,53 @@ const AccountPasswordChangeModal =({ close, isOpen, user }: AccountPasswordChang
             duplicateNewPassword: "",
         },
         onSubmit: v => {
-            setErrorMessage(null as React.JSX.Element);
-            // api
-        }
+            setErrorMessage(null);
+            getAccountApi(user).changePassword({
+                body: {
+                    email: user.email,
+                    oldPassword: v.currentPassword,
+                    newPassword: v.newPassword,
+                }
+            })
+                .then(d => {
+                    setBottomControl(<Typography>Your password has been changed!</Typography>);
+                })
+                .catch(e => {
+                    setErrorMessage(<Typography color="red">Password change failed! Check your current password!</Typography>);
+                })
+        },
+        validationSchema: Yup.object({
+            currentPassword: Yup.string()
+                .required("Current password is required"),
+            newPassword: Yup.string()
+                .min(8, "Password must be at least 8 characters")
+                .max(64, "Password must be less than 64 characters")
+                .matches(
+                    /^.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?].*$/,
+                    "Password requires at least one special character"
+                )
+                .matches(
+                    /^.*[a-z].*$/,
+                    "Password requires at least one lowercase letter"
+                )
+                .matches(
+                    /^.*[A-Z].*$/,
+                    "Password requires at least one uppercase letter"
+                )
+                .required("Password is required"),
+            duplicateNewPassword: Yup.string()
+                .required("Please confirm your new password")
+                .test("password-matches", "Password confirmation does not match", v => v === passwordChangeForm.values.newPassword)
+        }),
     });
+
+    useEffect(() => {
+        passwordChangeForm.values.currentPassword = "";
+        passwordChangeForm.values.newPassword = "";
+        passwordChangeForm.values.duplicateNewPassword = "";
+        setErrorMessage(null);
+        setBottomControl(submitButton);
+    }, [isOpen]);
 
     return (
         <Modal open={isOpen} onClose={close}>
@@ -42,6 +89,8 @@ const AccountPasswordChangeModal =({ close, isOpen, user }: AccountPasswordChang
                             value={passwordChangeForm.values.currentPassword}
                             onChange={passwordChangeForm.handleChange}
                             onBlur={passwordChangeForm.handleBlur}
+                            error={passwordChangeForm.errors.currentPassword !== undefined}
+                            helperText={passwordChangeForm.errors.currentPassword}
                             sx={{ mt: 2 }}
                         />
                         <TextField
@@ -53,6 +102,8 @@ const AccountPasswordChangeModal =({ close, isOpen, user }: AccountPasswordChang
                             value={passwordChangeForm.values.newPassword}
                             onChange={passwordChangeForm.handleChange}
                             onBlur={passwordChangeForm.handleBlur}
+                            error={passwordChangeForm.errors.newPassword !== undefined}
+                            helperText={passwordChangeForm.errors.newPassword}
                             sx={{ mt: 2 }}
                         />
                         <TextField
@@ -60,15 +111,18 @@ const AccountPasswordChangeModal =({ close, isOpen, user }: AccountPasswordChang
                             type="password"
                             id="duplicateNewPassword"
                             name="duplicateNewPassword"
-                            label="Re-enter New Password"
+                            label="Confirm New Password"
                             value={passwordChangeForm.values.duplicateNewPassword}
                             onChange={passwordChangeForm.handleChange}
                             onBlur={passwordChangeForm.handleBlur}
+                            error={passwordChangeForm.errors.duplicateNewPassword !== undefined}
+                            helperText={passwordChangeForm.errors.duplicateNewPassword}
                             sx={{ mt: 2 }}
                         />
                     </Grid2>
+                    { errorMessage ? <Grid2 container sx={{ mt: 2, p: 1}}><Typography>{errorMessage}</Typography></Grid2> : null}
                     <Grid2 container sx={{ flexGrow: 1, p: 1 }} alignItems="center">
-                        <Button color="primary" variant="contained" type="submit" sx={{ mt: 2 }}>Change Password</Button>
+                        {bottomControl}
                     </Grid2>
                 </form>
             </Box>
