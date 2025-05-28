5aee27de75a68545924179089d56e9b85758d8f3
Michael Quigley <michael@quigley.com>
2025-05-28 13:50:17 -0400

    store enrollment detail (#967)

---
+++ agent/enrollment.go
@@ -0,0 +1,38 @@
+package agent
+
+import (
+	"encoding/json"
+	"github.com/pkg/errors"
+	"os"
+)
+
+const EnrollmentV = "1"
+
+type Enrollment struct {
+	V           string `json:"v"`
+	ServiceName string `json:"service_name"`
+}
+
+func LoadEnrollment(path string) (*Enrollment, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	e := &Enrollment{}
+	if err := json.Unmarshal(data, e); err != nil {
+		return nil, err
+	}
+	if e.V != EnrollmentV {
+		return nil, errors.Errorf("invalid enrollment version '%v'; expected '%v'", e.V, EnrollmentV)
+	}
+	return e, nil
+}
+
+func (e *Enrollment) Save(path string) error {
+	e.V = EnrollmentV
+	data, err := json.MarshalIndent(e, "", "  ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(path, data, 0600)
+}
