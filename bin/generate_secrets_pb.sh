6cf5c28f90f7fa5f2cbcabc4f04a74c740a03140
Michael Quigley <michael@quigley.com>
2025-06-16 11:39:22 -0400

    roughed in gprc service for secrets retrieval (#968)

---
+++ bin/generate_secrets_pb.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+go install \
+    github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway \
+    google.golang.org/protobuf/cmd/protoc-gen-go \
+    google.golang.org/grpc/cmd/protoc-gen-go-grpc
+
+protoc --go_out=. --go_opt=paths=source_relative \
+	--go-grpc_out=. --go-grpc_opt=paths=source_relative \
+	controller/secretsGrpc/secrets.proto
+
