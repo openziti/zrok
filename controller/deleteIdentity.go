b7316ba8b0f237e32c9ae177f63de1e3ca86b975
Michael Quigley <michael@quigley.com>
2025-09-17 12:06:58 -0400

    deleteIdentityHandler (#1054)

--- controller/deleteIdentity.go
+++ controller/deleteIdentity.go
@@ -1,12 +1,10 @@
 package controller
 
 import (
-	"context"
-	"time"
+	"fmt"
 
 	"github.com/go-openapi/runtime/middleware"
-	"github.com/openziti/edge-api/rest_management_api_client/identity"
-	"github.com/openziti/zrok/controller/zrokEdgeSdk"
+	"github.com/openziti/zrok/controller/automation"
 	"github.com/openziti/zrok/rest_model_zrok"
 	"github.com/openziti/zrok/rest_server_zrok/operations/admin"
 	"github.com/sirupsen/logrus"
@@ -26,23 +24,21 @@ func (h *deleteIdentityHandler) Handle(params admin.DeleteIdentityParams, princi
 		return admin.NewDeleteIdentityUnauthorized()
 	}
 
-	edge, err := zrokEdgeSdk.Client(cfg.Ziti)
+	ziti, err := automation.NewZitiAutomation(cfg)
 	if err != nil {
-		logrus.Errorf("error getting edge client: %v", err)
+		logrus.Errorf("error getting automation client: %v", err)
 		return admin.NewDeleteIdentityInternalServerError()
 	}
 
-	if err := zrokEdgeSdk.DeleteEdgeRouterPolicy(identityZId, edge); err != nil {
+	// delete edge router policy for the identity
+	erpFilter := fmt.Sprintf("name=\"%v\"", identityZId)
+	if err := ziti.EdgeRouterPolicies.DeleteWithFilter(erpFilter); err != nil {
 		logrus.Errorf("error deleting edge router policy: %v", err)
 		return admin.NewDeleteIdentityInternalServerError()
 	}
 
-	req := &identity.DeleteIdentityParams{
-		ID:      identityZId,
-		Context: context.Background(),
-	}
-	req.SetTimeout(30 * time.Second)
-	if _, err := edge.Identity.DeleteIdentity(req, nil); err != nil {
+	// delete the identity
+	if err := ziti.Identities.Delete(identityZId); err != nil {
 		logrus.Errorf("error deleting identity '%v': %v", identityZId, err)
 		return admin.NewDeleteIdentityInternalServerError()
 	}
