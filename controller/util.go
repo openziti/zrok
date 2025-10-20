4e141fa0da117ff5a698a07f59b421645c4d05e7
Michael Quigley <michael@quigley.com>
2025-10-20 14:10:58 -0400

    lint

--- controller/util.go
+++ controller/util.go
@@ -139,8 +139,6 @@ func buildFrontendEndpointsForShare(shareId int, shareToken string, deprecatedEn
 	// build frontend endpoints from the names
 	var frontendEndpoints []string
 	for _, sn := range shareNames {
-		// use NameInNamespace where namespace.name is the template and name.name is the token
-		// this replaces {token} in namespace.name with the actual name.name value
 		endpoint := util.NameInNamespace(sn.Name.Name, sn.NamespaceName)
 		frontendEndpoints = append(frontendEndpoints, endpoint)
 	}
