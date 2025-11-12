6ac9de6393c937583f00102807f148cd3cad81c2
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2025-11-12 18:23:18 -0500

    forbit using @site for the future

--- website/docusaurus-plugin-zrok-docs.ts
+++ website/docusaurus-plugin-zrok-docs.ts
@@ -2,15 +2,17 @@ import * as path from 'path';
 import type { PluginConfig } from '@docusaurus/types';
 
 export function zrokDocsPluginConfig(rootDir: string): PluginConfig {
-    const sbp = path.resolve(rootDir, 'sidebars.js');
-    console.log('zrokDocsPluginConfig: sbp=', sbp);
+    const zp = path.resolve(rootDir, 'docs');
+    const zsbp = path.resolve(rootDir, 'sidebars.js');
+    console.log('zrokDocsPluginConfig: zp=', zp);
+    console.log('zrokDocsPluginConfig: sbp=', zsbp);
     return [
         '@docusaurus/plugin-content-docs',
         {
             id: 'zrok',
-            path: path.resolve(rootDir, 'docs'),
-            routeBasePath: 'docs/zrok',
-            sidebarPath: sbp,
+            path: zp,
+            routeBasePath: 'zrok',
+            sidebarPath: zsbp,
             lastVersion: 'current',
             includeCurrentVersion: true,
             versions: {
@@ -18,6 +20,16 @@ export function zrokDocsPluginConfig(rootDir: string): PluginConfig {
                 '1.0': { label: '1.0', path: '1.0' },
                 '0.4': { label: '0.4', path: '0.4' },
             },
+            remarkPlugins: [
+                function forbidSite() {
+                    return (tree, file) => {
+                        const src = String(file);
+                        if (src.includes('@site')) {
+                            throw new Error(`[FORBIDDEN] @site is not allowed in docs - use @zrokroot.\nFile: ${file.path}`);
+                        }
+                    };
+                },
+            ],
         } as any,
     ];
 }
