10967d8a257f8ecb312dd2e25e6b38fa3ad90e6f
Michael Quigley <michael@quigley.com>
2026-03-06 14:38:37 -0500

    fix for build chunking

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,10 +4,38 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react-dom") || id.includes("react-router") || id.includes("@emotion") || id.match(/\/react\//)) {
+              return "vendor-react";
+            }
+            if (id.includes("@mui/x-charts") || id.includes("@mui/x-charts-vendor")) {
+              return "vendor-mui-charts";
+            }
+            if (id.includes("@mui/x-date-pickers")) {
+              return "vendor-mui-pickers";
+            }
+            if (id.includes("@mui")) {
+              return "vendor-mui";
+            }
+            if (id.includes("@xyflow") || id.includes("d3-hierarchy")) {
+              return "vendor-visualizer";
+            }
+            if (id.includes("material-react-table")) {
+              return "vendor-table";
+            }
+          }
+        },
+      },
+    },
+  },
   server: {
     proxy: {
       '/api/v2': {
-        target: 'https://api-v2.zrok.io',
+        target: 'http://localhost:18080',
         changeOrigin: true,
       }
     },
